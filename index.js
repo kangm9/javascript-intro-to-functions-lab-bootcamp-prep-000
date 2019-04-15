4fa01d2f0707199011b2e449412ea51b209c8a3c
Min-Sun Kang <kangm914@gmail.com>
2019-04-15 20:26:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
 function logShout(string) {
   console.log(string.toUpperCase())
 }
+
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
 function sayHiToGrandma(string) {
   console.log("I can't hear you!")
   return string.toLowerCase()
