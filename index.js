02b32c92e4a2351cfa66da48a17a16ddc71157ba
Min-Sun Kang <kangm914@gmail.com>
2019-04-15 20:33:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
+function sayHiToGrandma() {
   console.log("hello")
   return string.toLowerCase()
 }
