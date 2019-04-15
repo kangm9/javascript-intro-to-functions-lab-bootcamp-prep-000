7619184964159f54bb74259a6ecfe6f5b1239cda
Min-Sun Kang <kangm914@gmail.com>
2019-04-15 21:21:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-if (string === string.toLowerCase())
-return("I can't hear you!")
+  if (string === string.toLowerCase()) {
+    return("I can't hear you!")
+  }
 }
 
 function sayHiToGrandma(string) {
