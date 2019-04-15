cd1f7cef603b1d12fdb421e5023d0b8cc1867b1a
Min-Sun Kang <kangm914@gmail.com>
2019-04-15 20:44:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
 console.log('hello')
-return("I can't hear you!".if )
+return("I can't hear you!".if (string.))
 }
 
 function sayHiToGrandma(string) {
